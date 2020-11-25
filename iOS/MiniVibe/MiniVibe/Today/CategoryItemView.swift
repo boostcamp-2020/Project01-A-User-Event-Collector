4020586149d89ac8fd9d90331766ac70e2da624f
mike123789-dev <mike123789@g.skku.edu>
2020-11-25 22:26:29 +0900

    [iOS] feat: CategoryItemView 구현

---
+++ iOS/MiniVibe/MiniVibe/Today/CategoryItemView.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryItemView.swift
+//  MiniVibe
+//
+//  Created by 강병민 on 2020/11/25.
+//
+
+import SwiftUI
+
+struct CategoryItemView: View {
+    let item: CategoryItem
+    let mode: CategoryMode
+    var itemSize: CGFloat {
+        switch mode {
+        case .full:
+            return UIScreen.main.bounds.width - 50
+        case .half:
+            return UIScreen.main.bounds.width/2 - 30
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "play.circle")
+                .resizable()
+                .cornerRadius(5)
+                .padding()
+                .frame(width: itemSize, height: itemSize)
+            if let title = item.title,
+               let description = item.description {
+                Text(title)
+                    .font(.headline)
+                Text(description)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
