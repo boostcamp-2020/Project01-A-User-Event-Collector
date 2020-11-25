063a6966f050e9df36a540f1c0c8fe4fdb2f1d2d
= <yeonduing@gmail.com>
2020-11-25 22:34:38 +0900

    [iOS] feat: RouterProtocol 작성
    페이지를 이동할 때 각 뷰에서 판단하지 않고 뷰 이동을 관리하는 라우터 객체를 분리하고 싶은데 아직 어떻게 해야할지 정확히 모르겠음
    학습이 더 필요함

---
+++ iOS/MiniVibe/MiniVibe/Playlist/RouterProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  RouterProtocol.swift
+//  MiniVibe
+//
+//  Created by 류연수 on 2020/11/25.
+//
+
+import Foundation
+
+protocol RoutingDestinationProtocol {
+    
+}
+
+protocol RouterProtocol {
+    func title() -> String
+    func route()
+}
