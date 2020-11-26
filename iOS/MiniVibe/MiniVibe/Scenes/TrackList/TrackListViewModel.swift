94173c8bcaa45f80b605be27ed524315bd7fbab9
mike123789-dev <mike123789@g.skku.edu>
2020-11-26 22:31:04 +0900

    fix: track list 화면 데이터 수정

--- iOS/MiniVibe/MiniVibe/Scenes/TrackList/TrackListViewModel.swift
+++ iOS/MiniVibe/MiniVibe/Scenes/TrackList/TrackListViewModel.swift
@@ -11,9 +11,9 @@ class TrackListViewModel: ObservableObject {
     @Published var tracks = [Track]()
     
     func fetchTracks() {
-        self.tracks = [Track(id: 1, title: "Dyanamite", artist: "방탄소년단", isFavorite: true),
-                       Track(id: 2, title: "Blooming", artist: "아이유", isFavorite: false),
-                       Track(id: 3, title: "Feel Good", artist: "프로미스나인", isFavorite: true),
+        self.tracks = [Track(id: 1, title: "Dynamite", artist: "방탄소년단", isFavorite: true),
+                       Track(id: 2, title: "Blueming", artist: "아이유", isFavorite: false),
+                       Track(id: 3, title: "FeelGood", artist: "프로미스나인", isFavorite: true),
                        Track(id: 4, title: "기억을 걷는 시간", artist: "넬", isFavorite: false),
                        Track(id: 5, title: "우산", artist: "윤하", isFavorite: true),
                        Track(id: 6, title: "나의 사춘기에게", artist: "볼빨간사춘기", isFavorite: false),
