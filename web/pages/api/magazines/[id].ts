1e992dd8b3d84f01b481560b4df5a3194d287637
sw15_sunkyu <azon731@gmail.com>
2020-11-26 20:01:41 +0900

    eslint test

--- web/pages/api/magazines/[id].ts
+++ web/pages/api/magazines/[id].ts
@@ -1,31 +1,31 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import {getMagazinePageData} from '../../../utils/test'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getMagazinePageData } from '../../../utils/test';
 
-const handler = async (_req:NextApiRequest,res:NextApiResponse) => {
+const handler = async (_req:NextApiRequest, res:NextApiResponse) => {
   const {
-    query : { id :stringId },
-    method : method
-  } = _req
+    query: { id: stringId },
+    method,
+  } = _req;
   const id : number = +stringId;
-  
-  try{
-    switch(method){
+
+  try {
+    switch (method) {
       case 'GET':
         const result = await getMagazinePageData(id);
-        if(!result) {
-          res.status(400).json({statusCode: 400, message: 'Bad Request'});
+        if (!result) {
+          res.status(400).json({ statusCode: 400, message: 'Bad Request' });
           return;
         }
-        
-        res.status(200).json({'Magzines':result});
-        break
+
+        res.status(200).json({ Magzines: result });
+        break;
 
       case 'POST':
 
-      default :
-        res.end()
+      default:
+        res.end();
     }
-  }catch(err){ res.status(500).json({ statusCode: 500, message: err.message }) }
-}
+  } catch (err) { res.status(500).json({ statusCode: 500, message: err.message }); }
+};
 
 export default handler;
--- web/utils/test.ts
+++ web/utils/test.ts
@@ -1,116 +1,116 @@
-import {PrismaClient} from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const getTrackCardData = async (id :number) => {
-  const track : any = await prisma.tracks.findUnique({ 
-    where: { id: id },
-    include:{Albums:true} 
-  })
-  if(!track) return null;
+  const track : any = await prisma.tracks.findUnique({
+    where: { id },
+    include: { Albums: true },
+  });
+  if (!track) return null;
 
-  const artistIdArr = await prisma.artists_Tracks.findMany({where: { trackId: id }})
+  const artistIdArr = await prisma.artists_Tracks.findMany({ where: { trackId: id } });
   track.Artists = await Promise.all(
-    artistIdArr.map((elem) => prisma.artists.findUnique({ where: { id: elem.artistId } }))
+    artistIdArr.map((elem) => prisma.artists.findUnique({ where: { id: elem.artistId } })),
   );
 
-  return track
+  return track;
 };
 
-
 const getPlaylistPageData = async (id : number) => {
-  const playlist : any = await prisma.playlists.findUnique({where:{id:id}})
-  if(!playlist) return null;
-  
+  const playlist : any = await prisma.playlists.findUnique({ where: { id } });
+  if (!playlist) return null;
+
   const trackIdArr = await prisma.playlists_Tracks.findMany({
-    where:{playlistId:id},
-    orderBy: {playlistTrackNumber: 'asc'},
-  })
-  playlist.Tracks = await Promise.all( 
-    trackIdArr.map((elem) => getTrackCardData(elem.trackId))
-  )
-    
-
-  return playlist
-}
-  
-const getArtistPageData = async (id :number) => {
-  const artist : any = await prisma.artists.findUnique({where:{id:id}})
-  if(!artist) return null;
-  artist.Albums = await prisma.albums.findMany({where:{artistId:id}})
-  
-  const trackIdArr = await prisma.artists_Tracks.findMany({where:{artistId:id}})
-  artist.Tracks = await Promise.all(
-    trackIdArr.map((elem) => getTrackCardData(elem.trackId))
-    )
-  
-
-  return artist
-}
-
-const getMagazinePageData = async (id :number) => {
-  const magazine : any = await prisma.magazines.findUnique({ where: { id: id }}) 
-  if(!magazine) return null;
-
-  const trackIdArr : any = await prisma.playlists_Tracks.findMany({
-    where: {playlistId: magazine.playlistId},
-    orderBy: {playlistTrackNumber: 'asc'},
-  })
-  magazine.Tracks = await Promise.all(
-    trackIdArr.map((elem)=>getTrackCardData(elem.trackId))
+    where: { playlistId: id },
+    orderBy: { playlistTrackNumber: 'asc' },
+  });
+  playlist.Tracks = await Promise.all(
+    trackIdArr.map((elem) => getTrackCardData(elem.trackId)),
   );
 
-  return magazine
+  return playlist;
+};
+
+const getArtistPageData = async (id :number) => {
+  const artist : any = await prisma.artists.findUnique({ where: { id } });
+  if (!artist) return null;
+  artist.Albums = await prisma.albums.findMany({ where: { artistId: id } });
+
+  const trackIdArr = await prisma.artists_Tracks.findMany({ where: { artistId: id } });
+  artist.Tracks = await Promise.all(
+    trackIdArr.map((elem) => getTrackCardData(elem.trackId)),
+  );
+
+  return artist;
+};
+
+const getMagazinePageData = async (id :number) => {
+  const magazine : any = await prisma.magazines.findUnique({ where: { id } });
+  if (!magazine) return null;
+
+  const trackIdArr : any = await prisma.playlists_Tracks.findMany({
+    where: { playlistId: magazine.playlistId },
+    orderBy: { playlistTrackNumber: 'asc' },
+  });
+  magazine.Tracks = await Promise.all(
+    trackIdArr.map((elem) => getTrackCardData(elem.trackId)),
+  );
+
+  return magazine;
 };
 
 const getAlbumPageData = async (id:number) => {
-  const album : any = await prisma.albums.findUnique({where:{id:id}})
+  const album : any = await prisma.albums.findUnique({ where: { id } });
 
-  if(!album) return null;
+  if (!album) return null;
 
   const trackIdArr : any = await prisma.playlists_Tracks.findMany({
-    where: {playlistId: album.playlistId},
-    orderBy: {playlistTrackNumber: 'asc'},
-  })
+    where: { playlistId: album.playlistId },
+    orderBy: { playlistTrackNumber: 'asc' },
+  });
   album.Tracks = await Promise.all(
-    trackIdArr.map((elem)=>getTrackCardData(elem.trackId))
+    trackIdArr.map((elem) => getTrackCardData(elem.trackId)),
   );
-  
-  return album
-}
+
+  return album;
+};
 
 const getGenrePageData = async (id:number) => {
-  const genre : any = await prisma.genres.findUnique({where:{id:id}});
-  if(!genre) return null;
+  const genre : any = await prisma.genres.findUnique({ where: { id } });
+  if (!genre) return null;
 
-  const trackIds : any = await prisma.tracks_Genres.findMany({where:{genreId: id}});
+  const trackIds : any = await prisma.tracks_Genres.findMany({ where: { genreId: id } });
   genre.Tracks = await Promise.all(
-    trackIds.map((elem: any)=>getTrackCardData(elem.trackId))
+    trackIds.map((elem: any) => getTrackCardData(elem.trackId)),
   );
-  const albumIds: any = await prisma.albums_Genres.findMany({where:{genreId: id}});
+  const albumIds: any = await prisma.albums_Genres.findMany({ where: { genreId: id } });
   genre.Albums = await Promise.all(
-    albumIds.map((elem: any)=> prisma.albums.findMany({where:{id: elem.albumId}}))
+    albumIds.map((elem: any) => prisma.albums.findMany({ where: { id: elem.albumId } })),
   );
-  const artistIds : any = await prisma.artists_Genres.findMany({where:{genreId: id}});
+  const artistIds : any = await prisma.artists_Genres.findMany({ where: { genreId: id } });
   genre.Artists = await Promise.all(
-    artistIds.map((elem: any)=>prisma.artists.findMany({where:{id: elem.artistId}}))
+    artistIds.map((elem: any) => prisma.artists.findMany({ where: { id: elem.artistId } })),
   );
 
   return genre;
-}
+};
 
 const getNewsPageData = async (id:number) => {
-  const news : any = await prisma.news.findUnique({where:{id:id}})
-  if(!news) return null;
+  const news : any = await prisma.news.findUnique({ where: { id } });
+  if (!news) return null;
 
   const trackIdArr : any = await prisma.playlists_Tracks.findMany({
-    where: {playlistId: news.playlistId},
-    orderBy: {playlistTrackNumber: 'asc'},
-  })
+    where: { playlistId: news.playlistId },
+    orderBy: { playlistTrackNumber: 'asc' },
+  });
   news.Tracks = await Promise.all(
-    trackIdArr.map((elem)=>getTrackCardData(elem.trackId))
+    trackIdArr.map((elem) => getTrackCardData(elem.trackId)),
   );
 
-  return news
-}
+  return news;
+};
 
-export {getPlaylistPageData, getArtistPageData,getMagazinePageData,getAlbumPageData, getGenrePageData, getNewsPageData}
+export {
+  getPlaylistPageData, getArtistPageData, getMagazinePageData, getAlbumPageData, getGenrePageData, getNewsPageData,
+};
