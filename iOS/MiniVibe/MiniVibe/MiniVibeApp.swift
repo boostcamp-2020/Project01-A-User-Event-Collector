61fea919dec2b465c3bffd7ff3b89bfdb71c4775
= <yeonduing@gmail.com>
2020-11-26 15:36:17 +0900

    [iOS] chore: 파일 그룹화

--- iOS/MiniVibe/MiniVibe/MiniVibeApp.swift
+++ iOS/MiniVibe/MiniVibe/MiniVibeApp.swift
@@ -9,12 +9,11 @@ import SwiftUI
 
 @main
 struct MiniVibeApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            TodayView()
+                .preferredColorScheme(.dark)
         }
     }
 }
