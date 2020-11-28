3008b69e48373597d8ccba9b28671b3aae5f73f7
sw15_sunkyu <azon731@gmail.com>
2020-11-28 22:57:36 +0900

    [BE] feat: 네이버 OAuth 정보 dotenv설정
    dotenv로 NAVER OAuth client 정보 설정

--- web/pages/about.tsx
+++ web/pages/about.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 
-const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
+const AboutPage = () => {
+  const client_id = process.env.NAVER_CLIENT_ID;
+  const redirectURI = encodeURI("http://localhost:3000/api/auth/naver");
+  const state = process.env.NAVER_STATE;
+
+  const api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
+
+  return <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
     <p>This is the about page</p>
     <p>
@@ -12,6 +18,6 @@ const AboutPage = () => (
     </p>
     <a href={api_url}><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>
   </Layout>
-);
+}
 
 export default AboutPage;
--- web/pages/api/auth/naver/index.ts
+++ web/pages/api/auth/naver/index.ts
@@ -1,12 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import { profile } from "console";
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   const { code } = _req.query;
   const { state } = _req.query;
-  const clientId = "_Zb3IXlIQbCuWqAQYSfU";
-  const clientSecret = "1HyCoFA9lC";
+  const clientId = process.env.NAVER_CLIENT_ID;
+  const clientSecret = process.env.NAVER_CLIENT_SECRET;
   const redirectURI = encodeURI("http://localhost:3000/api/auth/naver");
   const accessTokenUrl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectURI}&code=${code}&state=${state}`;
 
