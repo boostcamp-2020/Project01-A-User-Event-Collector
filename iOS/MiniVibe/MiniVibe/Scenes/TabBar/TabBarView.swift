c997dfeed9f136d599e535f33849ae721526f07d
mike123789-dev <mike123789@g.skku.edu>
2020-11-28 21:03:21 +0900

    [iOS] feat: TabView추가
    playerViewModel을 environmentObject 로 추가

---
+++ iOS/MiniVibe/MiniVibe/Scenes/TabBar/TabBarView.swift
@@ -0,0 +1,43 @@
+//
+//  TabBarView.swift
+//  MiniVibe
+//
+//  Created by 강병민 on 2020/11/28.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    
+    @StateObject var playerViewModel =  PlayerViewModel()
+
+    var body: some View {
+        TabView {
+            TodayView()
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("First")
+                }
+                .overlay(NowPlayingView(viewModel: playerViewModel), alignment: .bottom)
+            Text("Another Tab")
+                .tabItem {
+                    Image(systemName: "2.square.fill")
+                    Text("Second")
+                }
+            Text("The Last Tab")
+                .tabItem {
+                    Image(systemName: "3.square.fill")
+                    Text("Third")
+                }
+        }
+        .environmentObject(playerViewModel)
+        .colorScheme(.dark)
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+        
+    }
+}
