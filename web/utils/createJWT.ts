5ad30f05e6f04eefd2ad08e28c88bc794d53c06a
Yu Taek <lcpnine@gmail.com>
2020-11-28 22:54:23 +0900

    [BE] feat: simple-jwt를 이용한 jwt 생성 함수

---
+++ web/utils/createJWT.ts
@@ -0,0 +1,9 @@
+import { encode } from 'jwt-simple';
+
+const createJWT = (payload: any) => {
+    const secret = process.env.JWT_SECRET;
+    if (typeof secret !== 'string') return encode(payload, "dotenv is not working");;
+    return encode(payload, secret);
+}
+
+export { createJWT };
