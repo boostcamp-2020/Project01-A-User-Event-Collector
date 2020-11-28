8195c50e85880474a3ec31aa332de337221ade21
= <yeonduing@gmail.com>
2020-11-28 21:18:06 +0900

    [iOS] feat: TrackListButtonView 생성

---
+++ iOS/MiniVibe/MiniVibe/Scenes/TrackList/TrackListButtonView.swift
@@ -0,0 +1,40 @@
+//
+//  TrackListButtonView.swift
+//  MiniVibe
+//
+//  Created by 류연수 on 2020/11/28.
+//
+
+import SwiftUI
+
+struct TrackListButtonView: View {
+    var body: some View {
+        HStack{
+            Button(action: {
+                // action
+            }) {
+                HStack {
+                    Image(systemName: "play.fill")
+                    Text("PLAY")
+                        .modifier(Title2())
+                }
+            }.modifier(TrackListButtonStyle())
+            Button(action: {
+                // action
+            }) {
+                HStack {
+                    Image(systemName: "shuffle")
+                    Text("SHUFFLE")
+                        .modifier(Title2())
+                }
+            }.modifier(TrackListButtonStyle())
+        }
+    }
+}
+
+struct TrackListButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackListButtonView()
+            .preferredColorScheme(.dark)
+    }
+}
