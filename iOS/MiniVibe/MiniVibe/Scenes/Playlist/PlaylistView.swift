1ee3d3d774c48550ffbf968e163f90f06c45369e
= <yeonduing@gmail.com>
2020-11-30 03:22:38 +0900

    [iOS] fix: Steaky Header 적용 및 뷰 커스텀을 위하여 List에서 LazyVGrid로 변경

--- iOS/MiniVibe/MiniVibe/Scenes/Playlist/PlaylistView.swift
+++ iOS/MiniVibe/MiniVibe/Scenes/Playlist/PlaylistView.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct PlaylistView: View {
     private let playlist = Playlist(id: 1, title: "Dynamite", imageUrl: "Dynamite", description: "아무 생각 없이 드라이브하며 기분전환 어쩌고 저쩌고 dkdkdkdkdk", createdAt: "어제 어쩌고", author: "VIBE")
+    private let trackListId: Int = 1
+    private let layout = [GridItem(.flexible())]
     
-    
+//    init(playlist: Playlist, trackListId: Int) {
+//        self.playlist = playlist
+//        self.trackListId = trackListId
+//    }
     
     var body: some View {
-        VStack {
-            TrackListHeaderView(playlist: playlist)
-            TrackListButtonView()
-            TrackListView(id: 1)
+        ScrollView {
+            LazyVGrid(columns: layout, pinnedViews: [.sectionHeaders]) {
+                TrackListHeaderView(playlist: playlist)
+                Section(header: TrackListButtonView()) {
+                    TrackListView(id: trackListId)
+                }
+            }
         }.padding()
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
