743e548aa7e92f2b62d67c7f15dd0630206b8fa3
Yu Taek <lcpnine@gmail.com>
2020-11-29 21:48:47 +0900

    [BE] fix: createJWT에 returning type 추가

--- web/backend/utils/createJWT.ts
+++ web/backend/utils/createJWT.ts
@@ -1,6 +1,6 @@
 import { encode } from "jwt-simple";
 
-const createJWT = (payload: any) => {
+const createJWT = (payload: any): string => {
   const secret = process.env.JWT_SECRET;
   if (typeof secret !== "string") return encode(payload, "dotenv is not working");
   return encode(payload, secret);
