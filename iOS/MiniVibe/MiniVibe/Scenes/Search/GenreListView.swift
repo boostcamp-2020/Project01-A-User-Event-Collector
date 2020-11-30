6a88d99c2fb5ea132f589e1919d27f72447e9ac4
= <yeonduing@gmail.com>
2020-12-01 03:41:19 +0900

    [iOS] feat: GenreListView 생성

---
+++ iOS/MiniVibe/MiniVibe/Scenes/Search/GenreListView.swift
@@ -0,0 +1,39 @@
+//
+//  GenreListView.swift
+//  MiniVibe
+//
+//  Created by 류연수 on 2020/12/01.
+//
+
+import SwiftUI
+
+struct GenreListView: View {
+    private let layout = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: layout,
+                      spacing: 20) {
+                GenreCellView()
+                GenreCellView()
+                GenreCellView()
+                GenreCellView()
+                GenreCellView()
+                GenreCellView()
+                GenreCellView()
+                GenreCellView()
+                GenreCellView()
+                GenreCellView()
+            }
+        }
+    }
+}
+
+struct GenreListView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenreListView()
+    }
+}
