b7bfaf8611013ad4e13a8d3fe732e23aa9d22798
sw15_sunkyu <azon731@gmail.com>
2020-11-30 21:00:48 +0900

    [FE] feat: storybook addons 설치
    @storybook/addon-essentials dev dependency 설치 및 적용

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,27 +1,28 @@
 const path = require('path');
 module.exports = {
     stories: ['../frontend/components/**/**/*.stories.tsx'],
-        webpackFinal: async config => {
-          config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            use: [
-              {
-                loader: require.resolve('awesome-typescript-loader'),
-                options:{
-                  configFileName: path.resolve(__dirname, './tsconfig.json')
-                }
-              },
-              // Optional
-              {
-                loader: require.resolve('react-docgen-typescript-loader'),
-                options:{
-                  tsconfigPath: path.resolve(__dirname, './tsconfig.json'),
+    addons: ['@storybook/addon-essentials'],
+    webpackFinal: async config => {
+      config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: require.resolve('awesome-typescript-loader'),
+            options:{
+              configFileName: path.resolve(__dirname, './tsconfig.json')
+            }
+          },
+          // Optional
+          {
+            loader: require.resolve('react-docgen-typescript-loader'),
+            options:{
+              tsconfigPath: path.resolve(__dirname, './tsconfig.json'),
 
-                }
-              },
-            ],
-          });
-          config.resolve.extensions.push('.ts', '.tsx');
-          return config;
-        },
-        };
+            }
+          },
+        ],
+      });
+      config.resolve.extensions.push('.ts', '.tsx');
+      return config;
+    },
+};
