a324e538acc21920d8b80df41bb0a8a238068c8b
= <yeonduing@gmail.com>
2020-12-01 03:03:30 +0900

    [iOS] feat: RectangleListView 생성

--- iOS/MiniVibe/MiniVibe/Scenes/Search/RectangleCellView.swift
+++ iOS/MiniVibe/MiniVibe/Scenes/Search/RectangleCellView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct RectangleCellView: View {
     var body: some View {
         ZStack(alignment: .bottomLeading) {
-            Image(systemName: "person.fill")
+            Image("Blueming")
                 .resizable()
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width/2)
             RectangleCellInfoView()
---
+++ iOS/MiniVibe/MiniVibe/Scenes/Search/RectangleListView.swift
@@ -0,0 +1,35 @@
+//
+//  RectangleListView.swift
+//  MiniVibe
+//
+//  Created by 류연수 on 2020/12/01.
+//
+
+import SwiftUI
+
+struct RectangleListView: View {
+    private let layout = [GridItem(.flexible())]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout,
+                      spacing: 20) {
+                HStack {
+                    RectangleCellView()
+                    RectangleCellView()
+                    RectangleCellView()
+                    RectangleCellView()
+                    RectangleCellView()
+                    RectangleCellView()
+                    RectangleCellView()
+                }
+            }
+        }
+    }
+}
+
+struct RectangleListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleListView()
+    }
+}
