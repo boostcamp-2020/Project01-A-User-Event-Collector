b505cb13dcd4aef1b1f21da51c64fa8f6091ae75
= <yeonduing@gmail.com>
2020-12-01 01:56:59 +0900

    [iOS] feat: SearchBarView 생성

---
+++ iOS/MiniVibe/MiniVibe/Scenes/Search/SearchBarView.swift
@@ -0,0 +1,59 @@
+//
+//  SearchBarView.swift
+//  MiniVibe
+//
+//  Created by 류연수 on 2020/12/01.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    @Binding var text: String
+    @State private var isEditing = false
+    
+    var body: some View {
+        HStack {
+            TextField("검색어를 입력해 주세요.", text: $text)
+                .padding(9)
+                .padding(.horizontal, 27)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 22)
+                        if !isEditing {
+                            Image(systemName: "music.note")
+                                .foregroundColor(.gray)
+                                .padding(.trailing, 22)
+                        }
+                    }
+                )
+            
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+                    
+                }) {
+                    Text("취소")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView(text: .constant(""))
+    }
+}
