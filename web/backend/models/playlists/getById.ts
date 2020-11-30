ebc4a76ed05d7d367fde04a4a3a4e466d34025a3
marulloc <marullo@naver.com>
2020-11-30 20:27:58 +0900

    [BE] feat: api 수정 요청 적용
    playlists의 작성자의 pk값 뿐만 아니라,
    string id도 넘겨달라는 요청 적용

--- web/backend/models/playlists/getById.ts
+++ web/backend/models/playlists/getById.ts
@@ -2,7 +2,10 @@ import prisma from "../../../prisma";
 import { getTrackCard } from "../tracks";
 
 const getPlaylistById = async (id: number): Promise<Object | null> => {
-  const playlist: any = await prisma.playlists.findUnique({ where: { id } });
+  const playlist: any = await prisma.playlists.findUnique({
+    where: { id },
+    include: { Users: true },
+  });
   if (!playlist) return null;
 
   const trackIdArr = await prisma.playlists_Tracks.findMany({
