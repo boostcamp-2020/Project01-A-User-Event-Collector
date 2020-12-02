ccfdc6b60e697b336df7ddd893754f90c382c016
marulloc <marullo@naver.com>
2020-12-02 09:03:52 +0900

    [FE] feat: dynamic routing for album page
    dynamic routing 기본 셋팅 완료.
    
    album/[pid] 경로에 대한 라우팅 처리 완료

---
+++ web/pages/albums/[pid].tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useRouter } from "next/router";
+import prisma from "../../prisma";
+
+const AlbumPage = ({ album }: any) => {
+  const router = useRouter();
+  const { pid } = router.query;
+  return (
+    <>
+      {console.log(pid)}
+      {console.log(album)}
+    </>
+  );
+};
+
+export default AlbumPage;
+
+export async function getStaticPath() {
+  const albums = [{ id: 1 }, { id: 2 }, { id: 3 }];
+  const paths = albums.map((album: any) => `/albums/${album.id}`);
+
+  return { paths, fallback: false };
+}
+
+export async function getServerSideProps({ params }: any) {
+  const album = await prisma.albums.findUnique({ where: { id: +params.pid } });
+  return { props: { album } };
+}
--- web/pages/today/index.tsx
+++ web/pages/today/index.tsx
@@ -1,105 +1,11 @@
 import Link from "next/link";
 import Layout from "../../frontend/components/Layout";
-
-import TrackList from "../../frontend/components/Tracklist";
 import Img from "../../frontend/components/Img";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
 
-    <TrackList
-      Tracks={[
-        {
-          id: 21,
-          trackName: "Oh my god",
-          albumTrackNumber: 1,
-          albumId: 4,
-          Albums: {
-            id: 4,
-            albumName: "I trust",
-            description: "편견이란 답답한 우리를 무너뜨린 ‘LION'의 귀환! ",
-            cover:
-              "https://musicmeta-phinf.pstatic.net/album/004/511/4511159.jpg?type=r360Fll&v=20200406175912",
-            artistId: 2,
-          },
-          Artists: [
-            {
-              id: 2,
-              artistName: "아이들",
-              cover:
-                "https://musicmeta-phinf.pstatic.net/album/004/511/4511159.jpg?type=r360Fll&v=20200406175912",
-            },
-          ],
-        },
-        {
-          id: 22,
-          trackName: "사랑해",
-          albumTrackNumber: 2,
-          albumId: 4,
-          Albums: {
-            id: 4,
-            albumName: "I trust",
-            description: "편견이란 답답한 우리를 무너뜨린 ‘LION'의 귀환! ",
-            cover:
-              "https://musicmeta-phinf.pstatic.net/album/004/511/4511159.jpg?type=r360Fll&v=20200406175912",
-            artistId: 2,
-          },
-          Artists: [
-            {
-              id: 2,
-              artistName: "아이들",
-              cover:
-                "https://musicmeta-phinf.pstatic.net/album/004/511/4511159.jpg?type=r360Fll&v=20200406175912",
-            },
-          ],
-        },
-        {
-          id: 23,
-          trackName: "Maybe",
-          albumTrackNumber: 3,
-          albumId: 4,
-          Albums: {
-            id: 4,
-            albumName: "I trust",
-            description: "편견이란 답답한 우리를 무너뜨린 ‘LION'의 귀환! ",
-            cover:
-              "https://musicmeta-phinf.pstatic.net/album/004/511/4511159.jpg?type=r360Fll&v=20200406175912",
-            artistId: 2,
-          },
-          Artists: [
-            {
-              id: 2,
-              artistName: "아이들",
-              cover:
-                "https://musicmeta-phinf.pstatic.net/album/004/511/4511159.jpg?type=r360Fll&v=20200406175912",
-            },
-          ],
-        },
-        {
-          id: 24,
-          trackName: "LION",
-          albumTrackNumber: 4,
-          albumId: 4,
-          Albums: {
-            id: 4,
-            albumName: "I trust",
-            description: "편견이란 답답한 우리를 무너뜨린 ‘LION'의 귀환! ",
-            cover:
-              "https://musicmeta-phinf.pstatic.net/album/004/511/4511159.jpg?type=r360Fll&v=20200406175912",
-            artistId: 2,
-          },
-          Artists: [
-            {
-              id: 2,
-              artistName: "아이들",
-              cover:
-                "https://musicmeta-phinf.pstatic.net/album/004/511/4511159.jpg?type=r360Fll&v=20200406175912",
-            },
-          ],
-        },
-      ]}
-    />
     <p>
       <Link href="/about">
         <a>About</a>
