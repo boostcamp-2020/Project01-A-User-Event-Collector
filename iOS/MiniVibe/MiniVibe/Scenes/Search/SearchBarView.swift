6dbfa1e9c15000fd4a1042f126ce8f830d23d547
= <yeonduing@gmail.com>
2020-12-03 03:48:11 +0900

    [iOS] fix: 검색 내용은 서치바뷰 자체에서 컨트롤하도록 변경
    - onCommit: TextField에서 리턴했을 때 동작
    - .sheet: 새로운 모달뷰 생성
    - 모달뷰가 아닌 그냥 뷰가 대체되도록 변경해야함

--- iOS/MiniVibe/MiniVibe/Scenes/Search/SearchBarView.swift
+++ iOS/MiniVibe/MiniVibe/Scenes/Search/SearchBarView.swift
@@ -8,39 +8,49 @@
 import SwiftUI
 
 struct SearchBarView: View {
-    @Binding var text: String
+    @State private var text: String = ""
     @State private var isEditing = false
+    @State private var isPushed = false
+    
+    init(defaultText: String) {
+        self.text = defaultText
+    }
     
     var body: some View {
         HStack {
-            TextField("검색어를 입력해 주세요.", text: $text)
-                .padding(9)
-                .padding(.horizontal, 27)
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-//                .padding(.horizontal, 10)
-                .onTapGesture {
-                    self.isEditing = true
-                }
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
+            TextField("검색어를 입력해 주세요.", text: $text) { isEditing in
+                self.isEditing = isEditing
+            } onCommit: {
+                isPushed = true
+            }.sheet(isPresented: $isPushed) {
+                SearchAfterView(searchText: text)
+            }
+            .padding(9)
+            .padding(.horizontal, 27)
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+            //                .padding(.horizontal, 10)
+            //                .onTapGesture {
+            //                    self.isEditing = true
+            //                }
+            .overlay(
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 12)
+                    if !isEditing {
+                        Image(systemName: "music.note")
                             .foregroundColor(.gray)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 12)
-                        if !isEditing {
-                            Image(systemName: "music.note")
-                                .foregroundColor(.gray)
-                                .padding(.trailing, 12)
-                        }
+                            .padding(.trailing, 12)
                     }
-                )
+                }
+            )
             
             if isEditing {
                 Button(action: {
-                    self.isEditing = false
-                    self.text = ""
-                    
+                    isEditing = false
+                    text = ""
                 }) {
                     Text("취소")
                 }
@@ -54,6 +64,6 @@ struct SearchBarView: View {
 
 struct SearchBarView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchBarView(text: .constant(""))
+        SearchBarView(defaultText: "")
     }
 }
