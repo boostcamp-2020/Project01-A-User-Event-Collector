acde97698ae09c18e7a31f3895f3aa1b3a548dec
mike123789-dev <mike123789@g.skku.edu>
2020-12-02 20:04:41 +0900

    [iOS] fix: Player 화면 layout 수정

--- iOS/MiniVibe/MiniVibe/Scenes/Player/PlayerView.swift
+++ iOS/MiniVibe/MiniVibe/Scenes/Player/PlayerView.swift
@@ -10,102 +10,38 @@ import SwiftUI
 struct PlayerView: View {
     @ObservedObject var viewModel : PlayerViewModel
     @Binding var showMediaPlayer: Bool
-
+    @State var timeDuration = Float(180)
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.vertical, showsIndicators: false) {
-                VStack() {
+                VStack(spacing: 10) {
                     PlayerHeaderView(showMediaPlayer: $showMediaPlayer)
-                    Spacer()
-                    PlayerInfoView(track: viewModel.currentTrack)
-                    Spacer()
+                    PlayerInfoView(track: viewModel.currentTrack, timeDuration: $timeDuration)
                     PlayerControlView(viewModel: viewModel)
                 }
+                .padding(.horizontal, 20)
+                .padding(.bottom, geometry.safeAreaInsets.bottom)
                 .frame(height: geometry.size.height)
-                //TODO: 밑에 재생 queue에 있는 노래 목록 보여주기
-            }
-            .frame(height: geometry.size.height)
-        }
-    }
-}
 
-struct PlayerInfoView: View {
-    let track: Track
-    
-    var body: some View {
-        VStack(spacing: 40) {
-            AsyncImage(url: URL(string: track.album.cover ?? ""))
-                .padding()
-            HStack {
-                VStack (alignment: .leading){
-                    Text(track.trackName)
-                        .modifier(Title2())
-                    Text(track.album.name)
-                        .modifier(Description2())
+                LazyVGrid(columns: [GridItem(.flexible())]) {
+                    ForEach(viewModel.queue) { track    in                     TrackCellView(track: track)                    }
+                    Rectangle()
+                        .clearBottom()
                 }
-                Spacer()
-                Image(systemName: "ellipsis")
-                    .accesoryModifier(color: .gray, size: .medium)
+                .padding(.horizontal, 20)
+                
             }
-            .padding(.horizontal, 10)
+            .background(Color(UIColor.systemBackground))
+            .preferredColorScheme(.dark)
         }
+        .edgesIgnoringSafeArea(.bottom)
     }
-}
-
-struct PlayerControlView: View {
-    @ObservedObject var viewModel : PlayerViewModel
-    
-    var body: some View {
-        VStack {
-            HStack(spacing: 20) {
-                Button(action: {}, label: {
-                    Image(systemName: "repeat")
-                        .resizeToFit(padding: 15)
-                })
-                .accentColor(.primary)
-                Button(action: {}, label: {
-                    Image(systemName: "paperplane")
-                        .resizeToFit(padding: 5)
-                        .font(Font.title.weight(.light))
-                })
-                .accentColor(.primary)
-                Button(action: {}, label: {
-                    Image(systemName: "play")
-                        .resizeToFit(padding: 5)
-                        .font(Font.title.weight(.light))
-                })
-                .accentColor(.primary)
-
-                Button(action: {}, label: {
-                    Image(systemName: "heart")
-                        .resizeToFit(padding: 5)
-                })
-                .accentColor(.red)
-
-                Button(action: {}, label: {
-                    Image(systemName: "shuffle")
-                        .resizeToFit(padding: 15)
-                })
-                .accentColor(.primary)
-            }
-            HStack {
-                Spacer()
-                Button(action: {
-                    print(viewModel.queue)
-                }, label: {
-                    Image(systemName: "music.note.list")
-                        .accesoryModifier(color: .gray, size: .medium)
-                        .padding()
-                })
-            }
-        }
-    }
-    
 }
 
 struct PlayerHeaderView: View {
     @Binding var showMediaPlayer: Bool
-
+    
     var body: some View {
         HStack() {
             Image(systemName: "flame")
@@ -121,17 +57,112 @@ struct PlayerHeaderView: View {
                 
             })
         }
-        .padding()
     }
+}
+
+struct PlayerInfoView: View {
+    let track: Track
+    @Binding var timeDuration: Float
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            AsyncImage(url: URL(string: track.album.cover ?? ""))
+                .padding()
+            HStack() {
+                VStack(alignment: .leading, spacing: 10){
+                    Text(track.trackName)
+                        .font(.system(size: 24, weight: .bold))
+                    Text(track.artists.first?.name ?? "")
+                        .font(.system(size: 18, weight: .light))
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+                Image(systemName: "ellipsis")
+                    .accesoryModifier(color: .gray, size: .medium)
+            }
+            Slider(value: $timeDuration)
+                .padding(.bottom, 20.0)
+                .accentColor(.red)
+        }
+    }
+}
+
+struct PlayerControlView: View {
+    @ObservedObject var viewModel : PlayerViewModel
+    
+    var body: some View {
+        VStack() {
+            HStack() {
+                Button(action: {}, label: {
+                    Image(systemName: "repeat")
+                        .accesoryModifier(color: .gray, size: .medium)
+                })
+                .accentColor(.primary)
+                Spacer()
+                Button(action: {}, label: {
+                    Image(systemName: "paperplane")
+                        .accesoryModifier(color: .gray, size: .large)
+                    //                        .font(Font.title.weight(.light))
+                })
+                .accentColor(.primary)
+                Spacer()
+                Button(action: {}, label: {
+                    Image(systemName: "play")
+                        .accesoryModifier(color: .primary, size: .large)
+                        .font(Font.title.weight(.light))
+                })
+                .accentColor(.primary)
+                Spacer()
+                
+                Button(action: {}, label: {
+                    Image(systemName: "heart")
+                        .accesoryModifier(color: .red, size: .large)
+                })
+                .accentColor(.red)
+                Spacer()
+                
+                Button(action: {}, label: {
+                    Image(systemName: "shuffle")
+                        .accesoryModifier(color: .gray, size: .medium)
+                })
+                .accentColor(.primary)
+                
+            }
+            .padding(.vertical, 10)
+            HStack(alignment: .bottom) {
+                Button(action: {
+                    print(viewModel.queue)
+                }, label: {
+                    Image(systemName: "airplayaudio")
+                        .accesoryModifier(color: .gray, size: .medium)
+                })
+                
+                Spacer()
+                Text("미리듣기 중")
+                    .foregroundColor(.red)
+                Spacer()
+                Button(action: {
+                    print(viewModel.queue)
+                }, label: {
+                    Image(systemName: "music.note.list")
+                        .accesoryModifier(color: .gray, size: .medium)
+                })
+            }
+            .padding(.top, 20)
+            .padding(.bottom, 10)
+        }
+    }
+    
 }
 
 struct PlayerView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             PlayerView(viewModel: PlayerViewModel(), showMediaPlayer: .constant(true))
+                .colorScheme(.dark)
             //작은 화면 프리뷰
-//            PlayerView(viewModel: PlayerViewModel(), showMediaPlayer: .constant(true))
-//                .previewDevice("iPhone 8")
+            PlayerView(viewModel: PlayerViewModel(), showMediaPlayer: .constant(true))
+                .previewDevice("iPhone 8")
         }
     }
 }
