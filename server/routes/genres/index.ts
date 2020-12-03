681154b1430c19129bd1654f199c7853d082c48a
marulloc <marullo@naver.com>
2020-12-03 19:57:33 +0900

    [BE] feat: genre get
    interface 정의
    get all 메소드 정의
    get genre 메소드 정의 (하나만 가져오는 것)

--- server/routes/genres/index.ts
+++ server/routes/genres/index.ts
@@ -1,16 +1,8 @@
-import express from "express";
+import { Router } from "express";
+import genreController from "../../controllers/genres";
 
-const router = express.Router();
+const router = Router();
+router.get("/", genreController.getAll);
+router.get("/:id", genreController.getGenre);
 
-router.get("/", (req, res) => {
-  res.send("album index에요");
-});
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(id);
-});
-
-// 공부해서 바꾸자..
-const genresRouter = router;
-export default genresRouter;
+export default router;
