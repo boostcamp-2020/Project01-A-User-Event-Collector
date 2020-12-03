dfedc49254290acf8a37ee40691755a26a95e41a
marulloc <marullo@naver.com>
2020-12-03 15:07:27 +0900

    [BE] feat: eslint 적용
    server 프로젝트에 eslint 적용

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,16 @@
 import dotenv from "dotenv";
 import express from "express";
 
-process.env.NODE_ENV
-  ? dotenv.config({ path: ".env.production" })
-  : dotenv.config({ path: ".env.development" });
+if (process.env.NODE_ENV) {
+  dotenv.config({ path: ".env.production" });
+} else {
+  dotenv.config({ path: ".env.development" });
+}
 
 const app = express();
 
 app.get("/", (req, res) => res.send("asdasdd"));
 app.listen(process.env.PORT || 3000, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server Start on Stage: ${process.env.STAGE}`);
 });
