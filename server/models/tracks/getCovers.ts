08d5a6334456507b570a6feca1d61d186e8df0fa
marulloc <marullo@naver.com>
2020-12-04 02:32:38 +0900

    [BE] fix: api 수정 요청 적용
    track card가 아니라,
    track 정보(표지)만 주어질 때,
    album의 커버도 함께 넘겨달라는 요청 적용

--- server/models/tracks/getCovers.ts
+++ server/models/tracks/getCovers.ts
@@ -1,6 +1,12 @@
 import prisma from "../../prisma";
 
-const getTrackCovers = async (optObj: Object): Promise<Object> => {
+const getTrackCovers = async (optObj: any): Promise<Object> => {
+  // eslint-disable-next-line no-param-reassign
+  optObj.include = {
+    Albums: {
+      select: { cover: true },
+    },
+  };
   const result = await prisma.tracks.findMany(optObj);
   return result;
 };
