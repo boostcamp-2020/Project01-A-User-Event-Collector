c269d3f959990c71a09546a017c0fac6781d67d3
marulloc <marullo@naver.com>
2020-12-04 02:27:16 +0900

    [BE] fix: api 수정 요청 적용
    1. playlist 표지를 가져오는 과정에서
    작성자의  stringId도 포함해달라는 요청 적용

--- server/models/playlists/getCovers.ts
+++ server/models/playlists/getCovers.ts
@@ -1,6 +1,12 @@
 import prisma from "../../prisma";
 
-const getPlaylistCovers = async (optObj: Object): Promise<Object> => {
+const getPlaylistCovers = async (optObj: any): Promise<Object> => {
+  // eslint-disable-next-line no-param-reassign
+  optObj.include = {
+    Users: {
+      select: { username: true },
+    },
+  };
   const result = await prisma.playlists.findMany(optObj);
   return result;
 };
