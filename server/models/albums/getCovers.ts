df2818d711b9a3f87932e2f27bd10dccca187269
marulloc <marullo@naver.com>
2020-12-04 02:30:00 +0900

    [BE] fix: api 수정 요청 적용
    albums 표지를 가져올 때,
    외래키 artists Id 뿐만 아니라, join으로
    stringId도 넘겨달라는 요청 적용

--- server/models/albums/getCovers.ts
+++ server/models/albums/getCovers.ts
@@ -1,6 +1,12 @@
 import prisma from "../../prisma";
 
-const getAlbumCovers = async (optObj: Object): Promise<Object> => {
+const getAlbumCovers = async (optObj: any): Promise<Object> => {
+  // eslint-disable-next-line no-param-reassign
+  optObj.include = {
+    Artists: {
+      select: { artistName: true },
+    },
+  };
   const result = await prisma.albums.findMany(optObj);
   return result;
 };
