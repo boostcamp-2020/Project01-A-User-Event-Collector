e1be69f35861bc7f6aa9a77155dfd26ec7b19ba2
marulloc <marullo@naver.com>
2020-12-05 20:08:04 +0900

    [FE] feat: today page data fetch
    get Static props를 이용하여,
    build 시점에 데이터를 끌어오고 props로 데이터를 넘긴다.
    
    today 페이지 컴포넌트는 props를 구조분해할당한다.

--- frontend/pages/today/index.tsx
+++ frontend/pages/today/index.tsx
@@ -3,12 +3,13 @@ import { memo } from "react";
 import Img from "../../components/Img";
 import HotMagCard from "../../components/HotMagCard";
 import GlobalStyles from "../../components/GlobalStyles";
+import Card from "../../components/Card";
 
-const IndexPage = memo(() => (
+const IndexPage = memo(({ Magazines, News, Playlists }: any) => (
   <>
     <HotMagCard />
 
-    <p>
+    {/* <p>
       <Link href="/about">
         <a>About</a>
       </Link>
@@ -16,9 +17,36 @@ const IndexPage = memo(() => (
     <Img
       src="https://musicmeta-phinf.pstatic.net/album/005/094/5094136.jpg?type=r360Fll"
       varient="todayNews"
-    />
+    /> */}
     <GlobalStyles />
   </>
 ));
 
 export default IndexPage;
+
+export async function getStaticProps() {
+  const apiUrl = process.env.API_URL;
+  const apiPort = process.env.API_PORT;
+
+  // const magazineProms = fetch(`${apiUrl}:${apiPort}`);
+  // const newsProms = fetch(`${apiUrl}:${apiPort}`);
+  // const playlistProms = fetch(`${apiUrl}:${apiPort}`);
+
+  const resolveArr = await Promise.all([
+    fetch(`${apiUrl}:${apiPort}/api/magazines?limit=1`),
+    fetch(`${apiUrl}:${apiPort}/api/news?limit=1`),
+    fetch(`${apiUrl}:${apiPort}/api/playlists?limit=1`),
+  ]);
+  const result = await Promise.all(resolveArr.map((resolve) => resolve.json()));
+  const { Magazines } = result[0];
+  const { News } = result[1];
+  const { Playlists } = result[2];
+
+  return {
+    props: {
+      Magazines: Magazines,
+      News: News,
+      Playlists: Playlists,
+    },
+  };
+}
