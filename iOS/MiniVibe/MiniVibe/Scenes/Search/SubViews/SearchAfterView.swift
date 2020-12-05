f00e957cb16a10057b3e8a154f4548f7be7a603b
mike123789-dev <mike123789@g.skku.edu>
2020-12-05 16:44:33 +0900

    [iOS] refactor: sub view를 viewmodel과 연결
    기존view마다 갖고있던 state를 하나의 viewmodel로 통합

--- iOS/MiniVibe/MiniVibe/Scenes/Search/SubViews/SearchAfterView.swift
+++ iOS/MiniVibe/MiniVibe/Scenes/Search/SubViews/SearchAfterView.swift
@@ -8,39 +8,11 @@
 import SwiftUI
 
 struct SearchAfterView: View {
-    private let searchText: String
-    @StateObject private var viewModel = SearchAfterViewModel()
-    
-    init(searchText: String) {
-        self.searchText = searchText
-    }
+    @ObservedObject var viewModel: SearchViewModel
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [GridItem(.flexible())],
-                      spacing: 40,
-                      pinnedViews: [.sectionHeaders]) {
-                Section(header: SearchBarView(defaultText: searchText)) {
-                    if let tracks = viewModel.tracks {
-                        SearchAfterCategoryView(type: .track, cellDatas: tracks)
-                    }
-                    if let albums = viewModel.albums {
-                        SearchAfterCategoryView(type: .album, cellDatas: albums)
-                    }
-                    if let artists = viewModel.artists {
-                        SearchAfterCategoryView(type: .artist, cellDatas: artists)
-                    }
-                }
-            }
-        }.padding()
-        .onAppear {
-            viewModel.fetch(searchText: searchText)
-        }
-    }
-}
-
-struct SearchAfterView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchAfterView(searchText: "우기")
+        SearchAfterCategoryView(type: .track, cellDatas: viewModel.tracks)
+        SearchAfterCategoryView(type: .album, cellDatas: viewModel.albums)
+        SearchAfterCategoryView(type: .artist, cellDatas: viewModel.artists)
     }
 }
