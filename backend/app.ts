7a57535fa4f4ec702dbc1cd2d1ce985465c3e65d
utek <lcpnine@gmail.com>
2020-12-06 18:03:46 +0900

    [BE] feat: private auth check용 router 생성
    - private url을 먼저 체크하도록 app.ts에서 privateRouter를 상단으로 이동
    - private auth router 생성

--- backend/app.ts
+++ backend/app.ts
@@ -14,8 +14,8 @@ if (process.env.NODE_ENV) {
 const app = express();
 app.use(logger("short"));
 
+app.use("/api/private", privateRouter);
 app.use("/api", publicRouter);
-app.use("/api", privateRouter);
 
 app.listen(process.env.PORT || 4000, () => {
   // eslint-disable-next-line no-console
