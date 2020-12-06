8019240e2ae230d0e473b6e1558f0c8e9b252010
utek <lcpnine@gmail.com>
2020-12-06 21:35:39 +0900

    [BE] feat: private의 check를 UserInfo를 제공하는 함수로 변경

--- backend/controllers/private/auth/checkJWT/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-const checkJWT = (req: Request, res: Response): Response => {
-  return res.status(200).send({ message: "success" });
-};
-
-export default checkJWT;
