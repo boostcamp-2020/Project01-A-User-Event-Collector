da9339a564ef8742a18316157608c079c780e95d
utek <lcpnine@gmail.com>
2020-12-06 16:57:13 +0900

    [BE] feat: 서버 실행시 콘솔에 서버 주소 표기

--- backend/app.ts
+++ backend/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import dotenv from "dotenv";
 import express from "express";
 import logger from "morgan";
@@ -17,4 +18,10 @@ app.use("/api", apiRouter);
 app.listen(process.env.PORT || 4000, () => {
   // eslint-disable-next-line no-console
   console.log(`Server Start on Stage: ${process.env.STAGE}`);
+  if (process.env.NODE_ENV) {
+    console.log(`Server is on http://localhost:${process.env.PORT || 4000}`);
+    console.log(`And http://127.0.0.1:${process.env.PORT || 4000}`);
+  } else {
+    // TODO: Servery URL 올리기
+  }
 });
