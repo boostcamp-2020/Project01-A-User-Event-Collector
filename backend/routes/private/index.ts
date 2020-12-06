7ed5ca39a2e0ed7e517b163f3341b3ebfd0e7902
utek <lcpnine@gmail.com>
2020-12-06 17:46:53 +0900

    [BE] feat: public, private router 분리

---
+++ backend/routes/private/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const privateRouter = express.Router();
+
+privateRouter.use("");
+
+export default privateRouter;
--- backend/routes/public/search/index.ts
+++ backend/routes/public/search/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import searchController from "../../controllers/search";
+import searchController from "../../../controllers/search";
 
 const router = Router();
 router.get("/", searchController.search);
