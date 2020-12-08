039c6f9185947c79dd391ed1933721e8f2dcc8a6
marulloc <marullo@naver.com>
2020-12-09 02:06:34 +0900

    [FE] feat: reducer 적용
    all Check reducer를
    combine

--- frontend/reduxModules/index.ts
+++ frontend/reduxModules/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import checkedTrackReducer from "./checkedTrack";
+import allCheckedReducer from "./allCheck";
 
 export const rootReducer = combineReducers({
   checkedTrack: checkedTrackReducer,
+  AllCheckedFlag: allCheckedReducer,
 });
 
 // eslint-disable-next-line no-undef
