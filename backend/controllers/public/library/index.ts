672c26e0b58a98ec2bd68b5dd065c0b7ca89d121
marulloc <marullo@naver.com>
2020-12-09 07:33:44 +0900

    [BE] feat: library root controller
    뼈대 컨트롤러들을
    library index에 위치한
    root controller에 등록

--- backend/controllers/public/library/index.ts
+++ backend/controllers/public/library/index.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from "express";
-import { getLibArtists } from "./artists";
-import { getLibAlbums } from "./albums";
-import { getLibTracks } from "./tracks";
-import { getLibPlaylists } from "./playlsits";
+import { getLibArtists, postLibArtists } from "./artists";
+import { getLibAlbums, postLibAlbums } from "./albums";
+import { getLibTracks, postLibTracks } from "./tracks";
+import { getLibPlaylists, postLibPlaylists } from "./playlsits";
 
 interface Controller {
   getLibArtists(req: Request, res: Response): Promise<void>;
   getLibAlbums(req: Request, res: Response): Promise<void>;
   getLibTracks(req: Request, res: Response): Promise<void>;
   getLibPlaylists(req: Request, res: Response): Promise<void>;
+
+  postLibArtists(req: Request, res: Response): Promise<void>;
+  postLibAlbums(req: Request, res: Response): Promise<void>;
+  postLibTracks(req: Request, res: Response): Promise<void>;
+  postLibPlaylists(req: Request, res: Response): Promise<void>;
 }
 
 const controller: Controller = {
@@ -16,5 +21,9 @@ const controller: Controller = {
   getLibArtists,
   getLibTracks,
   getLibPlaylists,
+  postLibArtists,
+  postLibAlbums,
+  postLibTracks,
+  postLibPlaylists,
 };
 export default controller;
