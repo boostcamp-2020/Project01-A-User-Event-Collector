a069262e8a0252f809fe83662cfcd8afd25d0fa3
utek <lcpnine@gmail.com>
2020-12-09 00:26:59 +0900

    [FE] style: today page에 arrow icon 추가

---
+++ frontend/components/Slidebar/styled.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { SlidebarProps, TranslateProps } from "./index";
+
+const StyledSlidebar = styled.div<SlidebarProps>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+  & > a > svg {
+    width: 0.7rem;
+    height: 0.7rem;
+  }
+`;
+
+const SlideContainer = styled.div`
+  overflow-x: hidden;
+  overflow-y: hidden;
+  width: 100%;
+  position: relative;
+`;
+
+const StyledTitle = styled.div`
+  font-size: 1.3em;
+  font-weight: bold;
+  margin: 1em 0em;
+`;
+
+const SlideContent = styled.ul<TranslateProps>`
+  display: flex;
+  & > li:first-child {
+    margin: 0;
+  }
+  padding-inline-start: 0;
+  transition: all 0.6s ease-in-out;
+  transform: ${({ currentTranslateX }) => `translateX(${currentTranslateX}px)`};
+`;
+
+const StyledIcon = styled.span`
+  margin-left: 0.3em;
+`;
+
+export { StyledSlidebar, SlideContainer, StyledTitle, SlideContent, StyledIcon };
