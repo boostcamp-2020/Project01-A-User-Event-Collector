866cd285ac6767ee0a125d99d4579c834793ee3d
mike123789-dev <mike123789@g.skku.edu>
2020-12-08 13:20:12 +0900

    [iOS] feat: Reachability 추가

---
+++ iOS/MiniVibe/MiniVibe/Common/Reachability/ReachabilityObserverDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  ReachabilityObserverDelegate.swift
+//  MiniVibe
+//
+//  Created by 강병민 on 2020/12/07.
+// https://gist.github.com/popcornomnom/aa06d98f8d9834101a634f358e524660#file-reachabilityobserverdelegate-swift 참고
+
+import Foundation
+import Reachability
+
+//Reachability
+//declare this property where it won't go out of scope relative to your listener
+fileprivate var reachability: Reachability!
+
+protocol ReachabilityActionDelegate {
+    func reachabilityChanged(_ isReachable: Bool)
+}
+
+protocol ReachabilityObserverDelegate: class, ReachabilityActionDelegate {
+    func addReachabilityObserver() throws
+    func removeReachabilityObserver()
+}
+
+// Declaring default implementation of adding/removing observer
+extension ReachabilityObserverDelegate {
+    
+    /** Subscribe on reachability changing */
+    func addReachabilityObserver() throws {
+        reachability = try Reachability()
+        
+        reachability.whenReachable = { [weak self] reachability in
+            self?.reachabilityChanged(true)
+        }
+        
+        reachability.whenUnreachable = { [weak self] reachability in
+            self?.reachabilityChanged(false)
+        }
+        
+        try reachability.startNotifier()
+    }
+    
+    /** Unsubscribe */
+    func removeReachabilityObserver() {
+        reachability.stopNotifier()
+        reachability = nil
+    }
+    
+    func getConnection() -> Reachability.Connection {
+        return reachability.connection
+    }
+}
