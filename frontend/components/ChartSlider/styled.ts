586cfe1d8a8c68024a4e99bf0527c16626331717
utek <lcpnine@gmail.com>
2020-12-09 00:22:20 +0900

    [FE] style: 차트 페이지에 arrow icon 추가

---
+++ frontend/components/ChartSlider/styled.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+const StyledSlidebar = styled.div<Props>`
+  display: flex;
+  flex-direction: column;
+  width: 70rem;
+  & > a > svg {
+    width: 0.7rem;
+    height: 0.7rem;
+  }
+  &:nth-child(n) {
+    padding-top: 1.5em;
+    padding-bottom: 2.5em;
+    border-bottom: 0.1em solid rgba(0, 0, 0, 0.05);
+  }
+`;
+
+const StyledSlidebarTitle = styled.div`
+  font-size: 1.2em;
+  font-weight: bold;
+  margin-top: 1em;
+  margin-bottom: 0.2em;
+`;
+
+const SlideContainer = styled.div`
+  overflow-x: hidden;
+  overflow-y: hidden;
+  margin: 1em 0em;
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const SlideContent = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(20, minmax(50%, auto));
+  grid-template-rows: repeat(5, minmax(20%, auto));
+  grid-auto-flow: column;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  padding-inline-start: 0;
+`;
+
+const StyledIcon = styled.span`
+  margin-left: 0.3em;
+`;
+
+export { StyledSlidebar, StyledSlidebarTitle, SlideContainer, SlideContent, StyledIcon };
