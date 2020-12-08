6c3216d9723b7c747f2c367f16e55a2a0e2da5d4
utek <lcpnine@gmail.com>
2020-12-09 05:31:05 +0900

    [FE] style: 단위 변환이 된 라벨 재조절

--- frontend/components/MagLabel/MagLabel.style.ts
+++ frontend/components/MagLabel/MagLabel.style.ts
@@ -2,17 +2,16 @@ import styled from "styled-components";
 import { MagLabelStyles } from "./MagLabel.interface";
 
 const StyledMagLabel = styled.div<MagLabelStyles>`
-  height: 0.15em;
+  height: 0.5em;
   width: ${({ width }) => width}em;
   padding: 1em;
-  margin: 0.3em;
   background-color: ${({ backgroundColor, backgroundImage }) =>
     backgroundImage ? "" : backgroundColor};
   background-image: ${({ backgroundImage }) => backgroundImage};
   color: #fff;
   border-radius: 1.6em;
-  line-height: 0.15em;
-  font-size: 0.2em;
+  font-size: 0.9em;
+  line-height: 0.5em;
   text-align: center;
 `;
 
@@ -21,17 +20,17 @@ export default StyledMagLabel;
 const AllMagLabelStyles: MagLabelStyles = { width: 2, backgroundColor: "#FF0350" };
 
 const SpecialMagLabelStyles: MagLabelStyles = {
-  width: 4,
+  width: 6,
   backgroundImage: "linear-gradient(#e66465, #9198e5)",
 };
 
 const PickMagLabelStyles: MagLabelStyles = {
-  width: 2.2,
+  width: 3.3,
   backgroundColor: "#FF0350",
 };
 
 const GenreMagLabelStyles: MagLabelStyles = {
-  width: 3.2,
+  width: 4.8,
   backgroundColor: "#8B02ED",
 };
 
