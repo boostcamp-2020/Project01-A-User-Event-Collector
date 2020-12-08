7a2d7cd3cfd7d5eac4bd00d65b164ae99a525def
mike123789-dev <mike123789@g.skku.edu>
2020-12-08 13:21:56 +0900

    [iOS] feat: Coredata Mock Engine 구현

--- iOS/MiniVibe/MiniVibe/Common/Event/AnalyticsEngine.swift
+++ iOS/MiniVibe/MiniVibe/Common/Event/AnalyticsEngine.swift
@@ -14,7 +14,18 @@ protocol AnalyticsEngine: class {
 class MockAnalyticsEngine: AnalyticsEngine {
 
     func sendAnalyticsEvent(named name: String, metadata: [String: String]) {
-        print("\(name)")
+        print("MockServer - \(name)")
+        metadata.forEach { key, value in
+            print("ㄴ \(key) : \(value)")
+        }
+    }
+    
+}
+
+class BackupAnalyticsEngine: AnalyticsEngine {
+
+    func sendAnalyticsEvent(named name: String, metadata: [String: String]) {
+        print("BACKUP - \(name)")
         metadata.forEach { key, value in
             print("ㄴ \(key) : \(value)")
         }
