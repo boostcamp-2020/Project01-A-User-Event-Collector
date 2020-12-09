efda501102413c4b7ee7125963a919e7bc2dd095
marulloc <marullo@naver.com>
2020-12-09 20:15:52 +0900

    [BE] feat: user Liked items Controller
    user가 좋아요 한 album,track,playlists,artists를 반환하는 컨트롤러 작성,
    기존의 model 함수를 이용

---
+++ backend/controllers/public/userLikedItems/index.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import {
+  getUserLikeAlbums,
+  getUserLikeTracks,
+  getUserLikeArtists,
+  getUserLikePlaylists,
+} from "../../../models/library";
+
+const userLikedItems = async (req: Request, res: Response): Promise<void> => {
+  if (!req.user) {
+    res.json(403).json({});
+    return;
+  }
+  //   const userId = req.user.id;
+  const userId = 1;
+  const result = await Promise.all([
+    getUserLikeAlbums(+userId),
+    getUserLikeTracks(+userId),
+    getUserLikeArtists(+userId),
+    getUserLikePlaylists(+userId),
+  ]);
+
+  res.status(200).json({
+    LikedAlbums: result[0],
+    LikedTracks: result[1],
+    LikedArtists: result[2],
+    LikedPlaylists: result[3],
+  });
+};
+
+export default userLikedItems;
