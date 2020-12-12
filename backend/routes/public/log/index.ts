b06be4df36ea5cdb7467192d8459472e3cba66e9
utek <lcpnine@gmail.com>
2020-12-12 15:15:21 +0900

    [BE] feat: log get API 생성

--- backend/routes/public/log/index.ts
+++ backend/routes/public/log/index.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import logController from "../../../controllers/public/log";
 
 const router = Router();
+router.get("/web", logController.getWebLog);
+router.get("/ios", logController.getiOSLog);
 router.post("/web", logController.postWebLog);
 router.post("/ios", logController.postiOSLog);
 
