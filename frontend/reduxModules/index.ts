f1e01994c46203e585c9dd09b89a8c051a760c8c
utek <lcpnine@gmail.com>
2020-12-12 11:52:30 +0900

    [FE] refactor: redux의 사용하지 않는 allCheck 삭제

--- frontend/reduxModules/index.ts
+++ frontend/reduxModules/index.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import checkedTrackReducer from "./checkedTrack";
-import allCheckedReducer from "./allCheck";
 import playQueueReducer from "./playQueue";
 
 export const rootReducer = combineReducers({
   checkedTracks: checkedTrackReducer,
-  AllCheckedFlag: allCheckedReducer,
   playQueue: playQueueReducer,
 });
 
