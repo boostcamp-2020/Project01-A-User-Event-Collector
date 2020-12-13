df673062a3c51c5a5fa354aaee24ce16fdd49698
utek <lcpnine@gmail.com>
2020-12-13 15:16:51 +0900

    [BE] fix: like api를 put에서 post로 변경

--- backend/routes/public/tracks/index.ts
+++ backend/routes/public/tracks/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import trackController from "../../../controllers/public/tracks";
 
 const router = Router();
-router.put("/like", trackController.like);
-router.put("/unlike", trackController.unlike);
+router.post("/like", trackController.like);
+router.post("/unlike", trackController.unlike);
 
 export default router;
