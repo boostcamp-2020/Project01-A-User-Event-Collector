3fcb24210780f72f336d573bef2cb15db6c66d89
mike123789-dev <mike123789@g.skku.edu>
2020-12-14 14:35:30 +0900

    [iOS] test: 이벤트 수집이 제대로 이루어지고있는지 UI Test 코드 작성
    - 탭을 이용한 화면전환 이벤트 검증
    - 차트 화면에서의 이벤트 검증
    - 재생 관련 이벤트 검증
    - 검색 화면 이벤트 검증

--- iOS/MiniVibe/MiniVibeUITests/MiniVibeUITests.swift
+++ iOS/MiniVibe/MiniVibeUITests/MiniVibeUITests.swift
@@ -8,35 +8,88 @@
 import XCTest
 
 class MiniVibeUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        app = XCUIApplication()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    
+    func testAppLaunch() throws {
+        //앱 실행시 이벤트 수집이 제대로 이루어지는지 test
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.today)))
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testTabBarEvent() throws {
+        //TabBar에서 화면 전환 이벤트 제집이 제대로 이루어지는지 test
+        app.launch()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.today)))
+        app.staticTexts["차트"].tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.chart)))
+        app.staticTexts["검색"].tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.searchBefore)))
+        app.staticTexts["보관함"].tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.library)))
     }
+    
+    func testSearchViewEvent() throws {
+        //검색화면에서 이벤트 수집이 제대로 이루어지는지 test
+        app.launch()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.today)))
+        app.staticTexts["검색"].tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.searchBefore)))
+    }
+    
+    func testPlayerEvent() throws {
+        // 재생화면에서 이벤트 수집이 제대로 이루어지는지 test
+        app.launch()
+        let nowPlayingButton = app.buttons["NowPlayingView"]
+        let repeatButton = app.buttons["Repeat"]
+        let shuffleButton = app.buttons["Shuffle"]
+        let closePlayerButton = app.buttons["ClosePlayer"]
+        let pauseOrPlayButton = app.buttons["PauseOrPlay"]
+        
+        nowPlayingButton.tap()
+        _ = app.alerts.element.waitForExistence(timeout: 1)
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.playerPushed))
+        
+        repeatButton.tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(PlayerEvent.repeatOn))
+        shuffleButton.tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(PlayerEvent.shuffleOn))
+        repeatButton.tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(PlayerEvent.repeatOff))
+        shuffleButton.tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(PlayerEvent.shuffleOff))
+        pauseOrPlayButton.tap()
+        XCTAssertTrue(app.isDisplayingAlertWithOutMetaDataForEvent(PlayerEvent.trackPlayed(1)))
+        pauseOrPlayButton.tap()
+        XCTAssertTrue(app.isDisplayingAlertWithOutMetaDataForEvent(PlayerEvent.trackPaused(1)))
+        closePlayerButton.tap()
+        _ = app.alerts.element.waitForExistence(timeout: 1)
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.playerPopped))
+    }
+    
+    func testChartViewEvent() throws {
+        //차트화면에서 이벤트 수집이 제대로 이루어지는지 test
+        app.launch()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.today)))
+        app.staticTexts["차트"].tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewed(.chart)))
+        app.staticTexts["더보기"].tap()
+        XCTAssertTrue(app.isDisplayingAlertWithMetaDataForEvent(ScreenEvent.screenViewedWithSource(.playlist, source: .chart)))
+        
+        app.buttons["PLAY"].tap()
+        XCTAssertTrue(app.isDisplayingAlertWithOutMetaDataForEvent(PlayerEvent.trackPlayed(1)))
+        
+        app/*@START_MENU_TOKEN@*/.buttons["SHUFFLE"]/*[[".scrollViews.buttons[\"SHUFFLE\"]",".buttons[\"SHUFFLE\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.isDisplayingAlertWithOutMetaDataForEvent(PlayerEvent.trackPlayed(1)))
+    }
+    //test 완료
+    
 }
+
