3e214a0d50176c1f2b612a7d031e26f6c3b46ca8
mike123789-dev <mike123789@g.skku.edu>
2020-12-15 01:14:36 +0900

    [iOS] test: Scroll 기능 추가

--- iOS/MiniVibe/MiniVibeUITests/XCUIElement+Scroll.swift
+++ iOS/MiniVibe/MiniVibeUITests/XCUIElement+Scroll.swift
@@ -5,4 +5,17 @@
 //  Created by 강병민 on 2020/12/15.
 //
 
-import Foundation
+import XCTest
+
+extension XCUIElement {
+    func scrollToElement(element: XCUIElement) {
+        while !element.visible() {
+            swipeUp()
+        }
+    }
+
+    func visible() -> Bool {
+        guard self.exists && !self.frame.isEmpty else { return false }
+        return XCUIApplication().windows.element(boundBy: 0).frame.contains(self.frame)
+    }
+}
