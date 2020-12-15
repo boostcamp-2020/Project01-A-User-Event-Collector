4873d446653ac4163311fefc3a1a16aead8e6303
utek <lcpnine@gmail.com>
2020-12-15 20:43:35 +0900

    [FE] feat: 로그인 시 나는 에러 수정
    - type 관련 에러

--- frontend/components/Slidebar/index.tsx
+++ frontend/components/Slidebar/index.tsx
@@ -67,24 +67,27 @@ const Slidebar: React.FC<SlidebarProps> = ({
     const { current } = currentSlideRef;
     if (current !== null) {
       const containerWidth = Number(window.getComputedStyle(current).width.slice(0, -2));
-      const cardStyles = window.getComputedStyle(
-        current.firstElementChild?.nextElementSibling || new Element(),
-      );
-      const cardWidth = Number(cardStyles.width.slice(0, -2));
-      const cardMargin = Number(cardStyles.marginLeft.slice(0, -2));
-      const viewedCards = Math.floor(containerWidth / cardWidth);
-      const maxCardWidth = (cardWidth + cardMargin) * data.length - cardMargin - containerWidth;
-      setSlidePixels((cardWidth + cardMargin) * viewedCards);
-      if (nextHide && !previousHide) {
-        // 우측 끝에 붙어있었을 때
-        setCurrentTranslateX(-maxCardWidth);
-        return;
-      }
-      if (containerWidth >= (cardWidth + cardMargin) * data.length) {
-        setPreviousHide(true);
-        setNextHide(true);
-      } else {
-        setNextHide(false);
+      let cardStyles;
+      if (current.firstElementChild) {
+        if (current.firstElementChild?.nextElementSibling) {
+          cardStyles = window.getComputedStyle(current.firstElementChild.nextElementSibling);
+          const cardWidth = Number(cardStyles.width.slice(0, -2));
+          const cardMargin = Number(cardStyles.marginLeft.slice(0, -2));
+          const viewedCards = Math.floor(containerWidth / cardWidth);
+          const maxCardWidth = (cardWidth + cardMargin) * data.length - cardMargin - containerWidth;
+          setSlidePixels((cardWidth + cardMargin) * viewedCards);
+          if (nextHide && !previousHide) {
+            // 우측 끝에 붙어있었을 때
+            setCurrentTranslateX(-maxCardWidth);
+            return;
+          }
+          if (containerWidth >= (cardWidth + cardMargin) * data.length) {
+            setPreviousHide(true);
+            setNextHide(true);
+          } else {
+            setNextHide(false);
+          }
+        }
       }
     }
   };
