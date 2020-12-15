a0c011e38d4f5d706b97f62037ffd82560e8d16a
sw15_sunkyu <azon731@gmail.com>
2020-12-15 17:18:13 +0900

    [FE] 내 플레이리스트에 추가 모달 생성
    css미적용

---
+++ frontend/components/Modals/PlaylistSelector/row.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-expressions */
+import React, { FC } from "react";
+import styled from "styled-components";
+import HoverImg from "../../HoverImg";
+import { Playlist, Track } from "../../../interfaces";
+
+const StyledTrackCard = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: left;
+  width: 100%;
+  padding: 0.4rem 0rem;
+  &:hover {
+    background-color: #ededed;
+  }
+`;
+const StyledImg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PlaylistDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const PlaylistTitle = styled.div``;
+const NumOfTracks = styled.div`
+  color: #777777;
+`;
+
+interface Props {
+  data: Playlist;
+  tracks: Track[];
+}
+
+const ModalRow: FC<Props> = ({ data, tracks }: Props) => {
+  return (
+    <StyledTrackCard>
+      <StyledImg>
+        <HoverImg varient="trackCardCover" src={data.cover} />
+      </StyledImg>
+      <PlaylistDetails>
+        <PlaylistTitle>{data.playlistName}</PlaylistTitle>
+        <NumOfTracks>{data.author}</NumOfTracks>
+      </PlaylistDetails>
+    </StyledTrackCard>
+  );
+};
+
+export default ModalRow;
