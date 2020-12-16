c8ddc20ba3a92434ed35e5b2ba8edcaf5a46ce0f
utek <lcpnine@gmail.com>
2020-12-17 02:57:02 +0900

    [FE] style: 전체 화면의 width 비율 설정 및 body의 absolute position 설정

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-import React, { FC, memo, useEffect } from "react";
+import React, { FC, memo } from "react";
 import Layout from "../components/Layout";
 import { rootReducer } from "../reduxModules";
 
@@ -15,8 +15,10 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body{
-      position : relative;
-      width: 100vw;
+      position : absolute;
+      top: 0px;
+      left: 0px;
+      width: 100%;
       font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
       font-size: 14px;
       background-color:rgba(20,20,20,1);
