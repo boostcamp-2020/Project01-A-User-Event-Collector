143764a4f84e4a75c774562b75fecbd3f7243052
utek <lcpnine@gmail.com>
2020-12-17 13:30:37 +0900

    [FE] fix: onClick의 event type을 react의 MouseEvent tkdyd

--- frontend/components/Ellipsis/index.tsx
+++ frontend/components/Ellipsis/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { FC, useState } from "react";
+import React, { FC, MouseEvent } from "react";
 import styled from "styled-components";
 import icons from "../../constant/icons";
 
