672d09583a7d2bc06572b68c1f10638043a56525
utek <lcpnine@gmail.com>
2020-12-19 18:11:44 +0900

    [FE] feat: NavUser에 모달 추가

--- frontend/components/NavBar/NavBarUser/index.tsx
+++ frontend/components/NavBar/NavBarUser/index.tsx
@@ -1,8 +1,9 @@
 import React, { FC, memo, useState, useEffect } from "react";
-import Link from "next/link";
+import Router from "next/router";
 import Img from "../../Img";
+import icons from "../../../constant/icons";
 import myAxios from "../../../utils/myAxios";
-import { StyledNavUser, StyledUser } from "./styled";
+import { StyledNavUser, StyledUser, StyledModal, StyledModalCard, StyledIcon } from "./styled";
 
 interface Props {
   isLogged: boolean;
@@ -11,14 +12,39 @@ interface Props {
 
 const defaultProfile =
   "https://www.nailseatowncouncil.gov.uk/wp-content/uploads/blank-profile-picture-973460_1280.jpg";
+
 const defaultUserInfo = {
   userId: 0,
   username: "로그인",
   userProfile: defaultProfile,
 };
 
+interface Card {
+  title: string;
+  onClick?: Function;
+}
+
+const NavUserModal = ({ cards }: { cards: Card[] }): React.ReactElement => {
+  return (
+    <StyledModal>
+      {cards.map((card) => {
+        return (
+          <StyledModalCard key={card.title} onClick={card.onClick}>
+            {card.title}
+          </StyledModalCard>
+        );
+      })}
+    </StyledModal>
+  );
+};
+
 const NavBarUser: FC<Props> = memo(({ isLogged, setIsLogged }: Props) => {
   const [userInfo, setUserInfo] = useState(defaultUserInfo);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModal = (e: MouseEvent) => {
+    setShowModal(!showModal);
+  };
 
   const naverLoginURL = process.env.NEXT_PUBLIC_NAVER_LOGIN_URL || "today";
   useEffect(() => {
@@ -51,20 +77,50 @@ const NavBarUser: FC<Props> = memo(({ isLogged, setIsLogged }: Props) => {
     }
   }, [isLogged]);
 
+  const beforeLoggedInModalCards: Card[] = [
+    {
+      title: "NAVER 로그인",
+      onClick: () => {
+        Router.push(naverLoginURL);
+      },
+    },
+    {
+      title: "로컬 로그인",
+      onClick: () => {
+        Router.push("login");
+      },
+    },
+  ];
+
+  const afterLoggedInModalCards: Card[] = [
+    {
+      title: "로그아웃",
+      onClick: () => {
+        localStorage.removeItem("token");
+        setIsLogged(false);
+      },
+    },
+  ];
+
   return (
     <>
       {isLogged ? (
-        <StyledNavUser>
+        <StyledNavUser onClick={handleModal}>
           <Img varient="profile" src={userInfo.userProfile} />
-          <StyledUser isLogged={isLogged}>{userInfo.username}</StyledUser>
+          <StyledUser isLogged={isLogged}>
+            {userInfo.username}
+            <StyledIcon>{icons.sortDown}</StyledIcon>
+          </StyledUser>
+          {showModal ? <NavUserModal cards={afterLoggedInModalCards} /> : ""}
         </StyledNavUser>
       ) : (
-        <Link href={naverLoginURL}>
-          <StyledNavUser>
-            <Img varient="profile" src={defaultProfile} />
-            <StyledUser isLogged={isLogged}>로그인</StyledUser>
-          </StyledNavUser>
-        </Link>
+        <StyledNavUser onClick={handleModal}>
+          <Img varient="profile" src={defaultProfile} />
+          <StyledUser isLogged={isLogged}>
+            로그인<StyledIcon>{icons.sortDown}</StyledIcon>
+          </StyledUser>
+          {showModal ? <NavUserModal cards={beforeLoggedInModalCards} /> : ""}
+        </StyledNavUser>
       )}
     </>
   );
